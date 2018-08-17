19d0622f020a27e52dc3ef224c345c8516202ba2
qinzixin <qinzixin@fengbangleasing.com>
2018-08-17 11:13:47 +0800

    change

--- TestProject/VideoRecordViewController.m
+++ TestProject/VideoRecordViewController.m
@@ -8,12 +8,16 @@
 
 #import "VideoRecordViewController.h"
 #import "KVVideoRecorder.h"
+@import AVFoundation;
 @interface VideoRecordViewController ()
 @property (nonatomic,strong)KVVideoRecorder *recorder;
 @property (nonatomic,copy) NSString *fileName;
 @property (nonatomic,strong) UIView *videoView;
 @property (nonatomic,strong) UIView *bottomView;
 @property (nonatomic,strong) UIButton *recordButton;
+@property(nonatomic,strong) AVSpeechSynthesizer *voice;
+@property(nonatomic,strong) NSArray *stepContent;
+@property(nonatomic,strong) UITextField *textField;
 @end
 
 @implementation VideoRecordViewController
@@ -29,7 +33,12 @@
 -(void)viewDidLoad{
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
+    [self configData];
     [self configUI];
+}
+
+-(void)configData{
+    _stepContent = [[NSArray alloc]initWithObjects:@"第一步，打开冰箱",@"第二部，把大象放进去",@"第三部，关上冰箱", nil];
 }
 
 -(void)configUI{
@@ -52,14 +61,34 @@
 }
 
 -(void)configBottomView{
-    _recordButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 40, 20, 20)];
+    _recordButton = [[UIButton alloc]initWithFrame:CGRectMake(0, 160, 200, 20)];
     _recordButton.backgroundColor = [UIColor redColor];
+    [_recordButton setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+    [_recordButton setTitle:@"开始" forState:UIControlStateNormal];
     [_recordButton addTarget:self action:@selector(stopVideo) forControlEvents:UIControlEventTouchUpInside];
     [_bottomView addSubview:_recordButton];
+    
+    _textField = [[UITextField alloc]initWithFrame:CGRectMake(0, 0, 300, 100)];
+    _textField.backgroundColor = [UIColor grayColor];
+    [_bottomView addSubview:_textField];
 }
 
 -(void)stopVideo{
     [self.recorder stopCapture];
+    [self startVoice];
+}
+
+-(void)startVoice{
+    _voice = [[AVSpeechSynthesizer alloc]init];
+    AVSpeechUtterance *speech = [[AVSpeechUtterance alloc]initWithString:@"请大声听我朗读，一二三四五六七，上山打老虎"];
+    speech.volume = 1;
+    speech.rate = 0.5;
+    speech.pitchMultiplier = 1;
+    
+    AVSpeechSynthesisVoice *lauguage = [AVSpeechSynthesisVoice voiceWithLanguage:@"zh-CN"];
+    speech.voice = lauguage;
+    
+    [_voice speakUtterance:speech];
 }
 
 @end
